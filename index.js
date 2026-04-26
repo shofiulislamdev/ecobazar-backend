e5349e623cc2654377fbfd5f6dfcf0030a90446e
shofiulislamdev <shofiulislamsabbir@gmail.com>
2026-04-26 20:01:51 +0600

    add git ignore

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send("hello Developer")
+})
+
+app.listen(5000, ()=>{
+    console.log("Server running on port 5000")
+})
