3ae708b49f611abc3d2c66966fd51d1e330f4545
shofiulislamdev <shofiulislamsabbir@gmail.com>
2026-04-26 23:44:13 +0600

    configuration

---
+++ config/dbConfig.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const dbConfig = ()=> {
+    mongoose.connect(process.env.MONGODB_URL).then(()=>{
+        console.log("Database Connected")
+    })
+}
+
+module.exports = dbConfig
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const dbConfig = require('./config/dbConfig')
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+// Database config
+dbConfig()
 
 app.get('/', (req, res)=>{
     res.send("hello Developer")
 })
 
-app.listen(5000, ()=>{
-    console.log("Server running on port 5000")
+let port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
 })
